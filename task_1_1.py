e034df07e87480a7a82f005c17b4875f3b0b495d
Arthur <Arthur.tsoktoev@gmail.com>
2021-08-04 00:08:55 +0300

    Выполнил первое задание

--- task_1_1.py
+++ task_1_1.py
@@ -1,5 +1,31 @@
-<<<<<<< Updated upstream
-print('Hello world')
-=======
-print('Hello world 123')
->>>>>>> Stashed changes
+
+duration = 400153
+d_num = 86400
+h_num = 3600
+m_num = 60
+
+d = duration // d_num # Считает дни
+
+h1 = duration - (d_num * d)
+h = h1 // h_num # Считает часы
+
+m1 = h1 - (h_num * h)
+m = m1 // m_num # Считает минуты
+
+s = m1 - (m_num * m) # Считает секунды
+
+if m == 0:
+    print(f'{s} сек')
+elif h == 0:
+    print(f'{m} мин {s} сек')
+elif d == 0:
+    print(f'{h} час {m} мин {s} сек')
+elif d != 0:
+    print(f'{d} дн {h} час {m} мин {s} сек')
+
+
+
+
+
+
+
