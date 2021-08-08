b754624083b5acce9edb1547f770fbda329a60c0
Arthur <Arthur.tsoktoev@gmail.com>
2021-08-08 14:31:14 +0300

    Изменил программу

--- task_2_4.py
+++ task_2_4.py
@@ -1,20 +1,7 @@
-my_list = ['инженер-конструктор саня', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+my_list = ['инженер-конструктор саня', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита', 'программист питона АРтУР']
 
 for i in range(len(my_list)):
     new_list = my_list[i].split(" ")
-    ind = len(new_list) -1
-    list_name = new_list[ind]
-    name = list_name.lower().title()
-    my_list.append(name)
+    print(f'Привет {new_list[-1].title()}!')
 
-for i in range(1):
-    my_list.remove(my_list[i])
-for i in range(2):
-    my_list.remove(my_list[i])
-for i in range(1):
-    my_list.remove(my_list[i])
 
-print(f'Привет {my_list[0]}!')
-print(f'Привет {my_list[1]}!')
-print(f'Привет {my_list[2]}!')
-print(f'Привет {my_list[3]}!')
