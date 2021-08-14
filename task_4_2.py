fc79dfb68df448aa4f89e7d40e1c666d7e140d40
Arthur <Arthur.tsoktoev@gmail.com>
2021-08-14 18:57:33 +0300

    Задания выпонены

--- task_4_2.py
+++ task_4_2.py
@@ -1,36 +1,25 @@
 import requests
-from bs4 import BeautifulSoup as BS
+from bs4 import BeautifulSoup
 
 def currency_rates(user_char):
     r = requests.get('http://www.cbr.ru/scripts/XML_daily.asp')
-    html = BS(r.content, 'html.parser')
-    date_code = html.select('ValCurs')
-    date_code = str(date_code)
-    date = date_code[16:26]
-    for i in range(34):
-        currency_code = html.select('CharCode')[i]
-        value_code = html.select('Value')[i]
-        currency_code = str(currency_code)
-        currency = currency_code[10:13]
-        value_code = str(value_code)
-        value_code = value_code[7:14]
-        value_code_rub, value_code_kop = value_code.split(',')
-        value_code = f'{value_code_rub}.{value_code_kop}'
-        value_code = float(value_code)
-        if user_char == currency:
-            print(f'1 {currency}')
-            print(type(currency))
-            print(f'{value_code} RUB')
-            print(type(value_code))
-            print(date)
-            print(type(date))
+    soup = BeautifulSoup(r.text, 'lxml')
+    tags_char = soup.find_all(['charcode'])
+    tags_val = soup.find_all(['value'])
+    inx = 0
+    for tag in tags_char:
+        if tag.text == user_char:
+            val_rub, val_kop = tags_val[inx].text.split(',')
+            tags_val = f'{val_rub}.{val_kop}'
+            tags_val = float(tags_val)
+            print(type(tags_val))
+            print(tags_val)
             q = '+'
             break
         else:
+            inx += 1
             q = '-'
     if q == '-':
         print(None)
 
-
-
-currency_rates('HKD')
+currency_rates('USD')
