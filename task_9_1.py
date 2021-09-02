94a4f60c215bff6a274a939f68df23388c121e6f
Arthur <Arthur.tsoktoev@gmail.com>
2021-09-02 23:52:00 +0300

    Исправлены ошибки

--- task_9_1.py
+++ task_9_1.py
@@ -1,7 +1,8 @@
 from time import sleep
 
 class TrafficLight:
-    __color = {7:'Красный', 2:'Желтый', 5:'Зеленый'}
+    def __init__(self):
+        self.__color = {7:'Красный', 2:'Желтый', 5:'Зеленый'}
     def running_method(self):
         for i in self.__color:
             print(self.__color[i])
--- task_9_2.py
+++ task_9_2.py
@@ -3,8 +3,8 @@ class Road:
         self._lenght = length
         self._width = width
     def mass(self):
-        return self._lenght * self._width * 25 * 5
+        return self._lenght * self._width * 25 * 5 / 1000
 
 road = Road(20, 5000)
 
-print(road.mass())
+print(road.mass(), 'т')
